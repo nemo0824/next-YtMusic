652d4b37f727deb781d5cb323ff8d93463a92a61
임재원 <nemo0824@naver.com>
2025-03-23 02:59:22 +0900

    build : tracker-sdk version 1.0.9

--- app/TrackerWrapper.tsx
+++ app/TrackerWrapper.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect } from "react";
 import { tracker } from "tracker-sdk-nemo"
-
 export default function TrackerWrapper({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     tracker.init("bbd68f9e8ca71ab029fabeeb3fbd33d47bef425daaa5afe524376e1d0d304266");
