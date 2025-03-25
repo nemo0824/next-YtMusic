ee0f669e17f2a461eee1579e9cf95f452ffed883
임재원 <nemo0824@naver.com>
2025-03-25 13:02:35 +0900

    test: apikey변경

--- app/TrackerWrapper.tsx
+++ app/TrackerWrapper.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 import { tracker } from 'tracker-sdk-nemo'
 export default function TrackerWrapper({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    tracker.init("708952b9f1e86d86a67ed847dd437d3722874c9bb474d47552df380f2428837a");
+    tracker.init("6f17dff43ea5858cad770f7521a99170f90360b9fa6f943d481099b6e2efada5");
   }, []);
 
   return <>{children}</>;
